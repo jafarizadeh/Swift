33bc7ce7d858445effbd08bd509a3a9047dbe24b
Mehdi <mehdi0341@gmail.com>
2023-07-14 12:38:48 +0200

    Add new files for SpaceNews project

---
+++ SpaceNews/SpaceNews/SpaceNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsApp.swift
+//  SpaceNews
+//
+//  Created by Mehdi JAFARI ZADEH on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
