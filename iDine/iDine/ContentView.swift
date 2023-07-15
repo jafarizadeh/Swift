69452c99cbbf65341f6e6b7c2dc6f7f9a59e3d44
Mehdi <mehdi0341@gmail.com>
2023-07-15 10:53:32 +0200

    Srart

---
+++ iDine/iDine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Mehdi JAFARI ZADEH on 15/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Text("Mehdi")
+            Text("Mehdi")
+            Text("Mehdi")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Mehdi JAFARI ZADEH on 15/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
