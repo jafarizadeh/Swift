76f8c777772ddc22c8bb9589aabb1751cbd9404a
Mehdi <mehdi0341@gmail.com>
2023-07-16 00:14:01 +0200

    edit button

--- iDine/iDine/CheckoutView.swift
+++ iDine/iDine/CheckoutView.swift
@@ -19,6 +19,8 @@ struct CheckoutView: View {
     let tipAmounts = [10, 15, 20, 25, 0]
     @State private var tipAmount = 15
     
+    @State private var showingPaymentAlert = false
+    
     var totalPrice: String{
         let total = Double(order.total)
         let tipValue = total / 100 * Double(tipAmount)
@@ -50,12 +52,17 @@ struct CheckoutView: View {
             }
             Section("Total: \(totalPrice)"){
                 Button("Confirm Order"){
-                    // place the order
+                    showingPaymentAlert.toggle()
                 }
             }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order confirmed", isPresented: $showingPaymentAlert){
+            //add buttons here
+        } message:{
+        Text("Your total was \(totalPrice) - thank you!")
+        }
     }
 }
 
--- iDine/iDine/OrderView.swift
+++ iDine/iDine/OrderView.swift
@@ -21,6 +21,7 @@ struct OrderView: View {
                             Text("$\(item.price)")
                         }
                     }
+                    .onDelete(perform: deletItems)
                 }
                 
                 Section {
@@ -28,9 +29,17 @@ struct OrderView: View {
                         CheckoutView()
                     }
                 }
+                .disabled(order.items.isEmpty)
             }
             .navigationTitle("Order")
+            .toolbar{
+                EditButton()
+            }
         }
+    }
+    
+    func deletItems(at offsets: IndexSet){
+        order.items.remove(atOffsets: offsets)
     }
 }
 
