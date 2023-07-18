8e34c1171bb32ade1993cacd00cd8a9704ca5d07
Mehdi <mehdi0341@gmail.com>
2023-07-18 13:05:22 +0200

    Start Project WeSplit

---
+++ HackingWithSwift/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mehdi JAFARI ZADEH on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
