692bbe65156c1e115e48e2116b80ee494174a46c
Mehdi <mehdi0341@gmail.com>
2023-07-19 10:55:47 +0200

    ...

---
+++ HackingWithSwift/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Mehdi JAFARI ZADEH on 19/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(){
+            Text("Hello, world!")
+            Text("Hello, wordfdsld!")
+            Spacer()
+            Text("dfdf")
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
