58affef47a19c230762f7defc09e8d77d629aacf
Mehdi <mehdi0341@gmail.com>
2023-07-21 15:17:49 +0200

    Extensions

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Mehdi JAFARI ZADEH on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Mehdi JAFARI ZADEH on 21/07/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color ("Background")
+    static let icon = Color ("Icon")
+    static let text = Color ("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
