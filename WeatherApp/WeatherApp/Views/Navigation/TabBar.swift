f29c2713f2df144baf97af8c4b2ca1dd9334e5f2
Mehdi <mehdi0341@gmail.com>
2023-07-21 12:41:49 +0200

    tab bar

--- WeatherApp/WeatherApp/Views/Navigation/TabBar.swift
+++ WeatherApp/WeatherApp/Views/Navigation/TabBar.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct TabBar: View {
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // MARK: Tab Items
+            HStack {
+                // MARK: Expand Button
+                Button{
+                    action()
+                } label: {
+                    Image(systemName: "mappin.and.ellipse")
+                        .frame(width: 44, height: 44)
+                }
+                
+                Spacer()
+                
+                // MARK: Navigation Button
+                NavigationLink {
+                    
+                } label: {
+                    Image(systemName: "list.star")
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .font(.title2)
+            .foregroundColor(.white)
+            .padding(EdgeInsets(top: 20, leading: 32, bottom: 24, trailing: 32))
+        }
+        .frame(maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
     }
 }
 
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
-        TabBar()
+        TabBar(action: {})
+            .preferredColorScheme(.dark)
     }
 }
