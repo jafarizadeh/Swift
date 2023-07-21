4700ae94c63033db6ec9be0c94bbc3f7654d6834
Mehdi <mehdi0341@gmail.com>
2023-07-21 12:04:32 +0200

    home view

---
+++ WeatherApp/WeatherApp/Views/Navigation/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  WeatherApp
+//
+//  Created by Mehdi JAFARI ZADEH on 21/07/2023.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
