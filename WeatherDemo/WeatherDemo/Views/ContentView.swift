482d54d78ba0c29f352f0381a66bb32954e8700d
Mehdi <mehdi0341@gmail.com>
2023-07-22 16:35:48 +0200

    finish WelcomeView

---
+++ WeatherDemo/WeatherDemo/Views/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  WeatherDemo
+//
+//  Created by Mehdi JAFARI ZADEH on 22/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    var body: some View {
+        VStack {
+            WelcomeView()
+                .environmentObject(locationManager)
+        }
+        .background(Color(hue: 0.656,
+                          saturation: 0.787,
+                          brightness: 0.354))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
