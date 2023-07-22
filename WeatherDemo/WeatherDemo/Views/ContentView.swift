b4204455f32f860911ba2a26d230ea87221fd7d5
Mehdi <mehdi0341@gmail.com>
2023-07-22 17:04:41 +0200

    first Part

--- WeatherDemo/WeatherDemo/Views/ContentView.swift
+++ WeatherDemo/WeatherDemo/Views/ContentView.swift
@@ -12,8 +12,20 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            WelcomeView()
-                .environmentObject(locationManager)
+            
+            if let location = locationManager.location {
+                Text("Your coordinate are : \(location.longitude), \(location.latitude)")
+            }
+            else
+            {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+            
         }
         .background(Color(hue: 0.656,
                           saturation: 0.787,
