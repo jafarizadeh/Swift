57992a6dfa2beab91fe35b20f21a5375bc336399
Mehdi <mehdi0341@gmail.com>
2023-07-23 14:06:23 +0200

    Start

---
+++ ToDoDemo/ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by Mehdi JAFARI ZADEH on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
