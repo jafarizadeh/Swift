d5b8175bbcb60c55c691ff9e16604ab4c68c76b1
Mehdi <mehdi0341@gmail.com>
2023-07-24 19:12:28 +0200

    content view

---
+++ TriviaGameDemo/TriviaGameDemo/Views/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  TriviaGameDemo
+//
+//  Created by Mehdi JAFARI ZADEH on 24/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            VStack(spacing: 20){
+                Text("Trivia Game")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color("AccentColor"))
+                
+                Text("Are you ready to test out your trivia skills?")
+                    .foregroundColor(Color("AccentColor"))
+            }
+            
+            PrimaryButton(text: "Let's go!")
+            
+        }
+        .frame(maxWidth: .infinity,
+               maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.all)
+        .background(Color(red: 0.98,
+                        green: 0.93,
+                        blue:0.847))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
