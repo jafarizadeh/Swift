4b9c717247ae439918285dcd5525d88acf181289
Mehdi <mehdi0341@gmail.com>
2023-07-24 16:34:25 +0200

    Start

---
+++ AuthenticatorDemo/AuthenticatorDemo/AuthenticatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticatorDemoApp.swift
+//  AuthenticatorDemo
+//
+//  Created by Mehdi JAFARI ZADEH on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
