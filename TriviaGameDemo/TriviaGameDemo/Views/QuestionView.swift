10c08bd0d7529855db436b4fa5c5c6718660c049
Mehdi <mehdi0341@gmail.com>
2023-07-24 21:26:57 +0200

    Question View

---
+++ TriviaGameDemo/TriviaGameDemo/Views/QuestionView.swift
@@ -0,0 +1,49 @@
+//
+//  QuestionView.swift
+//  TriviaGameDemo
+//
+//  Created by Mehdi JAFARI ZADEH on 24/07/2023.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            HStack{
+                Text("Trivia Game")
+                    .lilacTitle()
+                Spacer()
+                Text("1 out of 10")
+                    .foregroundColor(Color("AccentColor"))
+                    .fontWeight(.heavy)
+            }
+            
+            ProgressBar(progress: 40)
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("MacOS is based on Linux.")
+                    .font(.system(size: 20))
+                    .bold()
+                    .foregroundColor(.gray)
+                
+                AnswerRow(answer: Answer(text: "false", isCorrect: true))
+                AnswerRow(answer: Answer(text: "true", isCorrect: false))
+            }
+            
+            PrimaryButton(text: "Next")
+        }
+        .padding()
+        .frame(maxWidth: .infinity,
+               maxHeight: .infinity)
+        .background(Color(red: 0.98,
+                        green: 0.93,
+                        blue:0.847))
+    }
+}
+
+struct QuestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionView()
+    }
+}
