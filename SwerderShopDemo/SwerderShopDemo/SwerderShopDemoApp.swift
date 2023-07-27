a059c0de72a8ecfced735a2bdccf35f0f4b78be8
Mehdi <jafarizadeh89@gmail.com>
2023-07-27 20:39:57 +0200

    ..

---
+++ SwerderShopDemo/SwerderShopDemo/SwerderShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwerderShopDemoApp.swift
+//  SwerderShopDemo
+//
+//  Created by Mehdi on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwerderShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
