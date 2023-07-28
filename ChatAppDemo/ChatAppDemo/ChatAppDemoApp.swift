0767c737a19babbb6fac65a47cc4f3e6f7bb71ec
Mehdi <jafarizadeh89@gmail.com>
2023-07-28 11:25:01 +0200

    start

---
+++ ChatAppDemo/ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Mehdi on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
