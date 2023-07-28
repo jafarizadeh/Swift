7c73d4ed16055c17a5bd3bf505cab61ec27862dd
Mehdi <jafarizadeh89@gmail.com>
2023-07-28 23:52:06 +0200

    titleRow

---
+++ Salle De Chat/Salle De Chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Salle De Chat
+//
+//  Created by Mehdi on 28/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color("Color1")
+            Text("Hello, world!")
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Salle De Chat/Salle De Chat/Salle_De_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Salle_De_ChatApp.swift
+//  Salle De Chat
+//
+//  Created by Mehdi on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Salle_De_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
