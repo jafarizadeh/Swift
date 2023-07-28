d8a9df1852398cce65f096730e94bd27c43fa230
Mehdi <jafarizadeh89@gmail.com>
2023-07-29 00:31:55 +0200

    message Bubble

---
+++ Salle De Chat/Salle De Chat/Component/MessageBubble.swift
@@ -0,0 +1,57 @@
+//
+//  MessageBubble.swift
+//  Salle De Chat
+//
+//  Created by Mehdi on 28/07/2023.
+//
+
+import SwiftUI
+
+struct MessageBubble: View {
+    var message: Message
+    var colorsArray = ["Color1", "Color2", "Color3", "Color4", "Color5"]
+
+    @State private var currentColor: String = "Color1"
+    @State private var textPosition: CGPoint = .zero
+    @State private var showTime = false
+
+    let textWidth: CGFloat = 200
+    let textHeight: CGFloat = 150
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                VStack {
+                    Text(message.text)
+                        .frame(width: textWidth, height: textHeight - 30)
+                        .padding()
+                    if showTime {
+                                Text("\(message.timestamp.formatted(.dateTime.hour().minute()))")
+                                    .frame(width: 100, alignment: .center)
+                                    .background(Color(.white).opacity(0.5))
+                                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                                    .padding(.bottom)
+                            }
+                }
+                .background(Color(currentColor))
+                .cornerRadius(30)
+                .position(textPosition)
+            }
+            .onAppear {
+                currentColor = colorsArray.randomElement() ?? "Color1"
+                let randomX = CGFloat.random(in: textWidth / 2..<geometry.size.width - textWidth / 2)
+                let randomY = CGFloat.random(in: textHeight / 2..<geometry.size.height - textHeight / 2)
+                textPosition = CGPoint(x: randomX, y: randomY)
+            }
+            .onTapGesture {
+                showTime.toggle()
+            }
+        }
+    }
+}
+
+struct MessageBubble_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageBubble(message: Message(id: "12345", text: "hello kjg  kugyufgiuy iughiug lkhkug 99", timestamp: Date()))
+    }
+}
