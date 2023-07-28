0e5c86ecffdfea6c272a81a66f8a824c101beee9
Mehdi <jafarizadeh89@gmail.com>
2023-07-28 11:18:55 +0200

    start

---
+++ MagicalGrid/MagicalGrid/MagicalGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicalGridApp.swift
+//  MagicalGrid
+//
+//  Created by Mehdi on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MagicalGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
