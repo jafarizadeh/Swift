4a4cd8434ad3ac5933313166cfa98ee6291ba3c9
Mehdi <jafarizadeh89@gmail.com>
2023-07-28 15:37:13 +0200

    message field

--- ChatAppDemo/ChatAppDemo/ContentView.swift
+++ ChatAppDemo/ChatAppDemo/ContentView.swift
@@ -11,18 +11,22 @@ struct ContentView: View {
     var messageArray = ["Hello you", "asdfea", "adfe", "", "asdf"]
     var body: some View {
         VStack {
-            TitleRow()
-            
-            ScrollView {
-                ForEach(messageArray, id: \.self) { text in
-                    MessageBubble(message: Message(id: "12345", text: text, received: true, timestamp: Date()))
+            VStack {
+                TitleRow()
+                
+                ScrollView {
+                    ForEach(messageArray, id: \.self) { text in
+                        MessageBubble(message: Message(id: "12345", text: text, received: true, timestamp: Date()))
+                    }
                 }
+                .padding(.top, 10)
+                .background(.white)
+                .cornerRadius(30, corners: [.topLeft, .topRight])
             }
-            .padding(.top, 10)
-            .background(.white)
-            .cornerRadius(30, corners: [.topLeft, .topRight])
+            .background(Color("Peach"))
+            
+            MessageField()
         }
-        .background(Color("Peach"))
         
     }
 }
