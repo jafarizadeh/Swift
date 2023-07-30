fbc0eb8ba77ed24e948dee0fdd1c59f2cf3f09ce
Mehdi <jafarizadeh89@gmail.com>
2023-07-30 16:24:27 +0200

    attributedString

---
+++ Weather/Weather/Views/Main/HomeView.swift
@@ -0,0 +1,78 @@
+//
+//  HomeView.swift
+//  Weather
+//
+//  Created by Mehdi on 30/07/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            // MARK: Background Color
+            Color.background
+                .ignoresSafeArea()
+            
+            // MARK: Background Image
+            Image("Background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            // MARK: Hous Image
+            Image("House")
+                .frame(maxHeight: .infinity,
+                       alignment: .top)
+                .padding(.top, 257)
+            
+            // MARK: Current Weather
+            VStack (spacing: -10) {
+                Text("Montreal")
+                    .font(.largeTitle)
+                
+                VStack {
+
+                    
+                    Text(attributedString)
+                    
+                    Text("H:24°   L:18°")
+                        .font(.title3.weight(.semibold))
+                }
+                
+                Spacer()
+                
+            }
+            .padding(.top, 51)
+        }
+    }
+    
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        
+        if let weather = string.range(of: "Mostly Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        
+        
+        
+        return string
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .preferredColorScheme(.dark)
+    }
+}
