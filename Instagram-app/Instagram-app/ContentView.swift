a914b2009a740c575d22ec2df67cf6d76c52fb84
Mehdi <jafarizadeh89@gmail.com>
2023-07-31 12:08:31 +0200

    Story

--- Instagram-app/Instagram-app/ContentView.swift
+++ Instagram-app/Instagram-app/ContentView.swift
@@ -9,18 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0.0) {
+            Header()
+            
+            Story()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Header: View {
+    var body: some View {
+        HStack {
+            Image("logo")
+            
+            
+            Spacer()
+            
+            HStack(spacing: 20.0) {
+                Image("add")
+                Image("heart")
+                Image("messenger")
+            }
+            
+        }
+        .padding(.horizontal, 15)
+        .padding(.vertical, 3)
+    }
+}
+
+struct Story: View {
+    var body: some View {
+        VStack {
+            VStack {
+                Image("profile")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .cornerRadius(50)
+            }
+            .overlay(
+                Circle()
+                    .stroke(LinearGradient(colors:[.red,
+                                                   .purple,
+                                                   .red,
+                                                   .orange,
+                                                   .yellow,
+                                                   .orange],
+                                           startPoint: .topLeading,
+                                           endPoint: .bottomTrailing),
+                            lineWidth: 2.3)
+                    .frame(width: 68, height: 68)
+            )
+            Text("Mehdi")
+                .font(.caption)
+        }
     }
 }
