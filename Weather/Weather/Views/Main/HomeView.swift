7dc3120f43ac3c8e764b2512e593c603da9ff949
Mehdi <jafarizadeh89@gmail.com>
2023-07-31 11:25:19 +0200

    bottom sgeet

--- Weather/Weather/Views/Main/HomeView.swift
+++ Weather/Weather/Views/Main/HomeView.swift
@@ -6,8 +6,16 @@
 //
 
 import SwiftUI
+import BottomSheet
+
+enum BottomSheetPosition: CGFloat, CaseIterable {
+    case top = 0.83 // 702 / 844
+    case middle = 0.385 // 325 / 844
+}
 
 struct HomeView: View {
+    @State var bottomSheetPosition: BottomSheetPosition = .middle
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -44,6 +52,12 @@ struct HomeView: View {
                     
                 }
                 .padding(.top, 51)
+                // MARK: Bottom Sheet
+                BottomSheetView(position: $bottomSheetPosition) {
+                    
+                } content: {
+                    
+                }
                 
                 // MARK: Tab Bar
                 TabBar(action: {})
