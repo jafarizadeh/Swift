8ee6844aa8417e5755d3d582220e77b50842734b
Mehdi <jafarizadeh89@gmail.com>
2023-08-05 00:03:10 +0200

    Patr 12

---
+++ DesignCodeiOS15/DesignCodeiOS15/HexagonView.swift
@@ -0,0 +1,39 @@
+//
+//  HexagonView.swift
+//  DesignCodeiOS15
+//
+//  Created by Mehdi on 04/08/2023.
+//
+
+import SwiftUI
+
+struct HexagonView: View {
+    var body: some View {
+        Canvas { context, size in
+            context.draw(
+                Text("DesignCode"),
+                at: CGPoint(x: 50, y: 20)
+            )
+            context.draw(
+                Image("Blob 1"),
+                in: CGRect(x: 0, y: 0, width: 200, height: 200)
+            )
+            context.draw(
+                Image(systemName: "hexagon.fill"),
+                in: CGRect(x: 0, y: 0, width: size.width, height: size.height)
+            )
+        }
+        .frame(width: 200, height: 212)
+        .foregroundStyle(.linearGradient(
+            colors: [.pink, .blue],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing)
+        )
+    }
+}
+
+struct HexagonView_Previews: PreviewProvider {
+    static var previews: some View {
+        HexagonView()
+    }
+}
