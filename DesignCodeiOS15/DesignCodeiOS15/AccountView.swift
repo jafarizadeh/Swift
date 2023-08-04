fb8891615aec6d5bac73e81ec6b6b685fff15f88
Mehdi <jafarizadeh89@gmail.com>
2023-08-04 17:17:59 +0200

    Patr 7

--- DesignCodeiOS15/DesignCodeiOS15/AccountView.swift
+++ DesignCodeiOS15/DesignCodeiOS15/AccountView.swift
@@ -11,12 +11,40 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
+                VStack (spacing: 8){
+                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundColor(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
+                    )
+                    Text("Mehdi JAFARI")
+                        .font(.title.weight(.semibold))
+                    
+                    HStack {
+                        Image(systemName: "location")
+                        Text("France")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                    
                 Section {
-                    Text("Setting")
+                    Label("Setting", systemImage: "gear")
                     
-                    Text("Billing")
+                    Label("Billing", systemImage: "creditcard")
                     
-                    Text("Help")
+                    Label("Help", systemImage: "questionmark")
+                        .imageScale(.small)
                 }
                 .listRowSeparator(.hidden)
 
