3caf4a8267fa06928c5841a590c6f255fd2900aa
Mehdi <jafarizadeh89@gmail.com>
2023-08-07 13:06:14 +0200

    Patr 36

--- DesignCodeiOS15/DesignCodeiOS15/Views/HomeView.swift
+++ DesignCodeiOS15/DesignCodeiOS15/Views/HomeView.swift
@@ -12,6 +12,7 @@ struct HomeView: View {
     @Namespace var namespace
     @State var show = false
     @State var showStatusBar = true
+    @State var selectedID = UUID()
     
     var body: some View {
         ZStack {
@@ -30,13 +31,17 @@ struct HomeView: View {
                     .padding(.horizontal, 20)
                 
                 if !show {
-                    CourseItem(namespace: namespace, show: $show)
-                        .onTapGesture {
-                            withAnimation(.openCard) {
-                                show.toggle()
-                                showStatusBar = false
-                            }
-                        }
+                    cards
+                } else {
+                    ForEach(courses) { course in
+                        Rectangle()
+                            .fill(.white)
+                            .frame(height: 300)
+                            .cornerRadius(30)
+                            .shadow(color: Color("Shadow"), radius: 20, x: 0, y: 10)
+                            .opacity(0.3)
+                        .padding(.horizontal, 30)
+                    }
                 }
                 
             }
@@ -49,13 +54,7 @@ struct HomeView: View {
         )
             
             if show{
-                CourseView(namespace: namespace, show: $show)
-                    .zIndex(1)
-                    .transition(.asymmetric(
-                        insertion: .opacity.animation(.easeInOut(duration: 0.1)),
-                        removal: .opacity.animation(.easeInOut(duration: 0.3).delay(0.2))
-                    
-                    ))
+            detail
             }
         }
         .statusBar(hidden: !showStatusBar)
@@ -88,7 +87,7 @@ struct HomeView: View {
     
     var featured: some View {
         TabView {
-            ForEach (courses) { course in
+            ForEach (featuredCourses) { course in
                 GeometryReader { proxy in
                     let minX = proxy.frame(in: .global).minX
                     
@@ -114,6 +113,35 @@ struct HomeView: View {
             Image("Blob 1")
                 .offset(x: 250, y: -100)
         )
+    }
+    
+    var cards: some View {
+        ForEach(courses) { course in
+            CourseItem(namespace: namespace,
+                       course: course,
+                       show: $show)
+                .onTapGesture {
+                    withAnimation(.openCard) {
+                        show.toggle()
+                        showStatusBar = false
+                        selectedID = course.id
+                    }
+            }
+        }
+    }
+    
+    var detail: some View {
+        ForEach(courses) { course in
+            if course.id == selectedID {
+                CourseView(namespace: namespace,
+                           course: course,
+                           show: $show)
+                .zIndex(1)
+                .transition(.asymmetric(
+                    insertion: .opacity.animation(.easeInOut(duration: 0.1)),
+                    removal: .opacity.animation(.easeInOut(duration: 0.3).delay(0.2))))
+            }
+        }
     }
 }
 
