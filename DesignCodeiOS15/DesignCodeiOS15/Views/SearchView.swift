c830cb1a4858e31908c1cc3b56ab68cb91cff3cc
Mehdi <jafarizadeh89@gmail.com>
2023-08-08 23:38:21 +0200

    ...

--- DesignCodeiOS15/DesignCodeiOS15/Views/SearchView.swift
+++ DesignCodeiOS15/DesignCodeiOS15/Views/SearchView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct SearchView: View {
     @State var text = ""
+    @State var show = false
+    @Namespace var namespace
+    @State var selectedIndex = 0
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
@@ -53,34 +56,42 @@ struct SearchView: View {
             .navigationBarItems(trailing: Button {
                 presentationMode.wrappedValue.dismiss()
             } label: {
-                Text ("Done").bold()
-                
-            })
+                Text ("Done").bold()})
+            .sheet(isPresented: $show) {
+                CourseView(namespace: namespace, course: courses[selectedIndex], show: $show)
+            }
         }
     }
     
     var content: some View {
-        ForEach(courses.filter{
-            $0.title.contains(text) || text == ""})
-        { item in
-            HStack (alignment: .top, spacing: 12){
-                Image(item.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 44, height: 44)
-                    .background(Color("Background"))
-                    .mask(Circle())
-                VStack (alignment: .leading, spacing: 4){
-                    Text(item.title).bold()
-                    Text(item.text)
-                        .font(.footnote)
-                        .foregroundColor(.secondary)
-                }
+        ForEach(Array(courses.enumerated()), id: \.offset)
+        { index, item in
+            if item.title.contains(text) || text == "" {
+                Button{
+                    show = true
+                    selectedIndex = index
+                } label: {
+                    HStack (alignment: .top, spacing: 12){
+                        Image(item.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 44, height: 44)
+                            .background(Color("Background"))
+                            .mask(Circle())
+                        VStack (alignment: .leading, spacing: 4){
+                            Text(item.title).bold()
+                                .foregroundColor(.primary)
+                            Text(item.text)
+                                .font(.footnote)
+                                .foregroundColor(.secondary)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .multilineTextAlignment(.leading)
+                        }
+                    }
+                    .padding(.vertical, 4)
+                    .listRowSeparator(.hidden)
             }
-            .padding(.vertical, 4)
-            .listRowSeparator(.hidden)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .multilineTextAlignment(.leading)
+            }
         }
     }
 }
