ad757ea9b4cfe42c5f79fa3c15835a1f93d7127b
Mehdi <jafarizadeh89@gmail.com>
2023-08-09 14:30:07 +0200

    part 4

---
+++ Messenger/Messenger/Core/Inbox/InbuxView.swift
@@ -0,0 +1,54 @@
+//
+//  InbuxView.swift
+//  Messenger
+//
+//  Created by Mehdi on 09/08/2023.
+//
+
+import SwiftUI
+
+struct InbuxView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                ActiveNowView()
+                
+                List {
+                    ForEach(0 ... 10, id:\.self) { message in
+                        InboxRowView()
+                    }
+                }
+                .listStyle(PlainListStyle())
+                .frame(height: UIScreen.main.bounds.height - 120)
+            }
+            .toolbar{
+                ToolbarItem (placement: .navigationBarLeading) {
+                    HStack {
+                        Image(systemName: "person.circle.fill")
+                        
+                        Text("Chats")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                }
+                
+                ToolbarItem (placement: .navigationBarTrailing) {
+                    Button {
+                        print("show new message view")
+                    } label: {
+                        Image(systemName: "square.and.pencil.circle.fill")
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                            .foregroundStyle(.black, Color(.systemGray5))
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct InbuxView_Previews: PreviewProvider {
+    static var previews: some View {
+        InbuxView()
+    }
+}
