9e2e7fa59a18f55f536f3c0288994b2987bb999a
Mehdi <jafarizadeh89@gmail.com>
2023-08-09 16:59:54 +0200

    part 7

---
+++ Messenger/Messenger/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewModel.swift
+//  Messenger
+//
+//  Created by Mehdi on 09/08/2023.
+//
+
+import SwiftUI
+import PhotosUI
+
+class ProfileViewModel: ObservableObject {
+    @Published var selectedItem: PhotosPickerItem? {
+        didSet { Task { try await loadImage() } }
+    }
+    
+    @Published var profileImage: Image?
+    
+    func loadImage() async throws {
+        guard let item = selectedItem else { return }
+        guard let imageData = try await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: imageData) else { return }
+        self.profileImage = Image(uiImage: uiImage)
+    }
+}
